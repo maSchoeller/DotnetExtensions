c8e31d4dd8ed4851edee75422012e3cfd3e87f4c
maSchoeller <info@marvinschoeller.de>
2019-11-28 22:33:56 +0100

    adding navigation, only small ideas

---
+++ src/MaSchoeller.Desktop.GenericHost.Extensions.WPF/Navigations/MsNavigationService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace MaSchoeller.Desktop.GenericHost.Extensions.WPF.Navigations
+{
+    public class MsNavigationService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IDictionary<Type, Type> _bindings;
+        private IServiceScope? _actualScope;
+
+        public MsNavigationService(IServiceProvider serviceProvider, IDictionary<Type, Type> bindings)
+        {
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            _bindings = bindings ?? throw new ArgumentNullException(nameof(bindings));
+        }
+
+
+        public void Navigate<TViewModel>()
+        {
+            Navigate(typeof(TViewModel));
+        }
+
+        public void Navigate(Type viewModel)
+        {
+            if (!_bindings.ContainsKey(viewModel))
+            {
+                throw new ArgumentException($"");
+            }
+            var viewType = _bindings[viewModel];
+            _actualScope?.Dispose();
+            _actualScope = _serviceProvider.CreateScope();
+
+            if (ActivatorUtilities.CreateInstance(_actualScope.ServiceProvider, viewType) is Page view)
+            {
+                view.DataContext = _actualScope
+                                        .ServiceProvider
+                                        .GetService(viewModel);
+                CurrentView.Navigate(view);
+            }
+            else
+            {
+                //Todo: add exception message
+                throw new InvalidCastException();
+            }
+
+        }
+
+        public Frame CurrentView { get; private set; }
+    }
+}
