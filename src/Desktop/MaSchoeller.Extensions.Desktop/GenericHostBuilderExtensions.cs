202b8437557da6f0b7c01105c7a9ad732a714147
maSchoeller <info@marvinschoeller.de>
2019-12-01 17:37:01 +0100

    update project and hostbuilder extensions

---
+++ src/Desktop/MaSchoeller.Extensions.Desktop/GenericHostBuilderExtensions.cs
@@ -0,0 +1,53 @@
+﻿using MaSchoeller.Extensions.Desktop.Abstracts;
+using MaSchoeller.Extensions.Desktop.Internals.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MaSchoeller.Extensions.Desktop
+{
+    public static class GenericHostBuilderExtensions
+    {
+        public static IHostBuilder UseSplashScreen<TSplashScreen>(this IHostBuilder builder)
+            where TSplashScreen : Window, ISplashscreenWindow, new()
+        {
+            var app = ApplicationBuilder
+                .CreateIfNotExistsAsync(ShutdownMode.OnMainWindowClose)
+                .Result;
+
+            app.Dispatcher.InvokeAsync(() =>
+            {
+                var window = new TSplashScreen();
+                window.Show();
+                builder.ConfigureServices((context,services) =>
+                {
+                    services.AddSingleton<ISplashscreenWindow>(window);
+                    services.AddHostedService<SplashscreenLifetimeHost>();
+                });
+            }).Wait();
+            return builder;
+        }
+
+        public static IHostBuilder ConfigureDesktopDefaults<TShellWindow>(this IHostBuilder builder, Action<IDesktopBuilder>? action = null)
+            where TShellWindow : Window, IDesktopShell
+        {
+            var wpfbuilder = new DesktopBuilder<TShellWindow>(builder);
+            action?.Invoke(wpfbuilder);
+            wpfbuilder.Build();
+            return builder;
+        }
+
+        public static IHostBuilder ConfigureDesktopDefaults(this IHostBuilder builder, Action<IDesktopBuilder>? action = null)
+        {
+            var wpfbuilder = new DesktopBuilder(builder);
+            action?.Invoke(wpfbuilder);
+            wpfbuilder.Build();
+            return builder;
+        }
+    }
+}
