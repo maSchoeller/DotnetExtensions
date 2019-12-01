c35db7e76ebe0def473216d530a3bb86920da821
maSchoeller <info@marvinschoeller.de>
2019-12-01 17:36:06 +0100

    Adding Abstractions

---
+++ src/Desktop/MaSchoeller.Extensions.Desktop/Abstracts/IDesktopBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace MaSchoeller.Extensions.Desktop.Abstracts
+{
+    public interface IDesktopBuilder
+    {
+        void ConfigureServices(Action<IServiceCollection> services);
+        void ConfigureApplication(Action<Application> application);
+        void UseStartup<TStartup>();
+
+        void Build();
+    }
+}
---
+++ src/Desktop/MaSchoeller.Extensions.Desktop/Abstracts/IDesktopContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MaSchoeller.Extensions.Desktop.Abstracts
+{
+    public interface IDesktopContext
+    {
+        ShutdownMode ShutdownMode { get; }
+
+        IHostApplicationLifetime Lifetime { get; }
+
+        bool IsRunning { get; }
+
+        Application? WpfApplication { get; }
+
+        Dispatcher? Dispatcher { get; }
+    }
+}
