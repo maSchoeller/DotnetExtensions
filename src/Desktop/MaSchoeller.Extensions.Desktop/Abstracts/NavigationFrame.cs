5d889b0ab28c49e7da55136f06ad52655c62ab63
maSchoeller <git@marvinschoeller.de>
2019-12-11 18:04:30 +0100

    update navigationframe

--- src/Desktop/MaSchoeller.Extensions.Desktop/Abstracts/NavigationFrame.cs
+++ src/Desktop/MaSchoeller.Extensions.Desktop/Abstracts/NavigationFrame.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace MaSchoeller.Extensions.Desktop.Abstracts
 {
     public class NavigationFrame : Frame
     {
+        public NavigationFrame()
+        {
+            NavigationUIVisibility = NavigationUIVisibility.Hidden;
+        }
     }
 }
